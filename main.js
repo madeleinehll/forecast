f7771b8b53cf7171ddba90b3a393f13c158ef173
madeleinehll <hill.madeleine13@yahoo.de>
2024-05-16 16:27:56 +0200

    icons aus unterordner, icons zur Wettervorhersage eingebunden

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ async function showForecast(url) {
         pointToLayer: function (feature, latlng) {
             let details = feature.properties.timeseries[0].data.instant.details;
             let time = new Date(feature.properties.timeseries[0].time);
-            console.log(details);
+            let symbol = feature.properties.timeseries[0].data.next_1_hours.summary.symbol_code;
             let content = `
             <h4>Wettervorhersage für ${time.toLocaleString()}</h4>
             <ul>
@@ -54,6 +54,11 @@ async function showForecast(url) {
                 <li>Windgeschwindigkeit (km/h): ${Math.round(details.wind_speed*3.6)}</li>
             </ul>
             `;
+            for(let i=0; i<=24; i+= 3){
+                let symbol = feature.properties.timeseries[i].data.next_1_hours.summary.symbol_code;
+                content += `<img src="icons/${symbol}.svg" alt="${symbol}" style ="width:32px">`
+                console.log(i,symbol);
+            }
             L.popup(latlng, {
                 content: content
             }).openOn(themaLayer.forecast);
